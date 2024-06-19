3a36320ecac30e67b35ed4b5a2ac4536a7939190
Rachit Shaha <72016334+Rachit-Shaha@users.noreply.github.com>
2024-06-18 23:50:01 -0400

    Time: 48 ms (6.85%) | Memory: 16.5 MB (72.51%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,43 @@
+class Solution:
+
+    def countPaths(self, i: int, j: int, n: int, m: int,dp: List[List[int]]) -> int:
+            if i >= n or j >= m:
+                return 0
+            if i == n-1 and j == m-1:
+                return 1
+            if dp[i][j] != -1:
+                return dp[i][j]
+            else:
+                dp[i][j] = self.countPaths(i+1,j,n,m,dp) + self.countPaths(i, j+1,n,m,dp)
+                return dp[i][j]
+
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[-1 for _ in range(n+1)]for _ in range(m+1)]
+        ans = self.countPaths(0,0,m,n,dp)
+        if m==1 and n==1:
+            return ans
+        return dp[0][0]
+    """
+    def countPaths(self, i: int, j: int, n: int, m: int, dp: List[List[int]]) -> int:
+        if i == (n - 1) and j == (m - 1):
+            return 1
+        if i >= n or j >= m:
+            return 0
+        if dp[i][j] != -1:
+            return dp[i][j]
+        else:
+            dp[i][j] = self.countPaths(
+                i + 1, j, n, m, dp) + self.countPaths(i, j + 1, n, m, dp)
+            return dp[i][j]
+
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[-1 for _ in range(n + 1)] for _ in range(m + 1)]
+
+
+        # dp[1][1]=1;
+        num = self.countPaths(0, 0, m, n, dp)
+        if m == 1 and n == 1:
+            return num
+        return dp[0][0]"""
+
+        
