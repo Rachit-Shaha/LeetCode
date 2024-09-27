ade8894f689235792dd82b4a85b7f24f5b823db3
Rachit Shaha <72016334+Rachit-Shaha@users.noreply.github.com>
2024-09-26 21:08:10 -0400

    Time: 54 ms (50.23%) | Memory: 16.7 MB (45.80%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ans = []
+        ds = []
+
+        def findComb(ind: int, target: int):
+            if (ind == len(candidates)):
+                if (target == 0):
+                    ans.append(ds[:])
+                return
+            if candidates[ind] <= target:
+                ds.append(candidates[ind])
+                findComb(ind, target - candidates[ind])
+                ds.pop()
+            findComb(ind+1, target)
+        findComb(0, target)
+        return ans
+
+        
